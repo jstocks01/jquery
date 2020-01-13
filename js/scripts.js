234806e015474627ec2189c12c672bcb4916ddf7
Jason Stocks and Todd Walraven <todd@>
2020-01-13 14:01:12 -0800

    add jquery event handlers to html

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+$("p").click(function() {
+    alert("This is a paragraph.");
+  });
+  
+$("img").click(function() {
+    alert("This is an image.");
+  });
+$("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you, THIS IS A HEADER!");
+  });
+});
